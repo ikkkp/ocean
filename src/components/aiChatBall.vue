91d2ec0414a71c9221d116bea6b8ee1c2e0004f4
ikkkp <2496450092@qq.com>
2023-11-26 13:37:45 +0800

    modified:   src/components/aiChatBall.vue 	modified:   src/config.js 	new file:   src/images/main-picture/great_blue_whale.png 	new file:   src/images/main-picture/great_blue_whale.svg

--- src/components/aiChatBall.vue
+++ src/components/aiChatBall.vue
@@ -1,13 +1,21 @@
+<!-- 
+ * @description: 
+ * @fileName: aiChatBall.vue 
+ * @author: Huangzl 
+ * @date: 2023-11-26 12:27:41
+ * @version: V1.0.0 
+!-->
+
 <!-- 可拖拽的小球 封装 -->
 <template>
-    <div ref="floatButton" class="backHome" @click="goCreatePage" :style="{
+    <div ref="floatButton" class="backHome" @click="goCreatePage()" :style="{
         width: itemWidth + 'px',
         height: itemHeight + 'px',
         left: left + 'px',
         top: top + 'px',
     }">
-        <v-btn class="ma-2" outlined large fab color="indigo">
-            <v-icon>mdi-pencil</v-icon>
+        <v-btn v-model="fab" class="ma-2" mid fab color="primary">
+            <v-img src="../images/main-picture/great_blue_whale.svg" style="width: 20px;"></v-img>
         </v-btn>
     </div>
 </template>
@@ -39,6 +47,7 @@ export default {
     },
     data() {
         return {
+            fab: false,
             top: 0,
             left: 0,
             currentTop: 0,
@@ -56,28 +65,32 @@ export default {
     mounted() {
         this.$nextTick(() => {
             const floatButton = this.$refs.floatButton;
-            floatButton.addEventListener('touchstart', () => {
+            floatButton.addEventListener('touchstart', (e) => {
                 floatButton.style.transition = 'none';
+                e.preventDefault(); // Disable scroll
             });
             // 在拖拽过程中，组件应该跟随手指的移动而移动
             floatButton.addEventListener('touchmove', (e) => {
+                e.preventDefault(); // Disable scroll
                 if (e.targetTouches.length === 1) {
-                    // 一根手指
+                    // 获取手指的平面位置
                     let touch = e.targetTouches[0];
                     this.left = touch.clientX - 20;
                     this.top = touch.clientY - 25;
+
                 }
             });
             // 拖拽结束后，重新调整组件的位置并重新设置过渡动画
             floatButton.addEventListener('touchend', () => {
                 floatButton.style.transition = 'all 0.3s';
                 if (this.left > document.documentElement.clientWidth / 2) {
-                    this.left =
-                        this.clientWidth - this.itemWidth - this.gapWidth;
-                    // this.left = document.documentElement.clientWidth - 60;
+                    this.left = this.clientWidth - this.itemWidth - this.gapWidth;
                 } else {
-                    this.left = 10;
-                    // this.left = 0;
+                    this.left = 20;
+                } if (this.top < 0) {
+                    this.top = 0;
+                } else if (this.top > this.clientHeight - this.itemHeight) {
+                    this.top = this.clientHeight - this.itemHeight - 80;
                 }
             });
         });
@@ -85,7 +98,7 @@ export default {
     methods: {
         // 返回首页菜单
         goCreatePage() {
-            this.$emit('goManage');
+            console.log('返回首页');
         },
     },
 };
